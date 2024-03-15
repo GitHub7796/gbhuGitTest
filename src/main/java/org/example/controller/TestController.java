5f3eab14927d7b28ee604ed5e57ecd6a84d3416f
ahu <2659813569@qq.com>
2024-03-15 15:13:46 +0800

    3

--- src/main/java/org/example/controller/TestController.java
+++ src/main/java/org/example/controller/TestController.java
@@ -3,7 +3,7 @@ package org.example.controller;
 public class TestController {
 
     public void hello() {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println("Hello World!2");
+        System.out.println("Hello World!2");
     }
 }
--- src/main/java/org/example/controller/hello2.java
+++ src/main/java/org/example/controller/hello2.java
@@ -3,6 +3,6 @@ package org.example.controller;
 public class hello2
 {
     public static void main(String[] args) {
-        System.out.println("Hello World2!");
+        System.out.println("Hello World2!2");
     }
 }
