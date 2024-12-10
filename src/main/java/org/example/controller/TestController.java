2b59be1ce2e71c8c44ec2247969df04d47499d42
ahu <2659813569@qq.com>
2024-12-10 15:42:17 +0800

    1211

--- src/main/java/org/example/controller/TestController.java
+++ src/main/java/org/example/controller/TestController.java
@@ -5,5 +5,6 @@ public class TestController {
     public void hello() {
         System.out.println("Hello World!2");
         System.out.println("Hello World!2");
+        System.out.println("Hello World!2");
     }
 }
